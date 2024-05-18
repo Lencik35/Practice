d58b4cc2a769309f8527a056b1785e0d1c483207
ESTOLEAR <stolearelena88@gmail.com>
2024-05-18 12:40:59 +0300

    1 test

---
+++ README.md
@@ -0,0 +1,6 @@
+### project1
+
+* Add dependencies
+* Create 'Hello World' test
+* Create testng.xml
+* 
--- src/test/java/Tests/TestRegres.java
+++ src/test/java/Tests/TestRegres.java
@@ -2,10 +2,17 @@ package Tests;
 
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class TestRegres {
     @Test
-    public void testTestNg() {
-        System.out.println("Hello World");
+    public void testListUsers() {
+      given()
+              .when()
+              .get("https://reqres.in/api/users?page=2")
+              .then()
+              .assertThat()
+              .statusCode(200);
 
 
     }
