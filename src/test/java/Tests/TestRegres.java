55483e6384b041150d68bfae68c7c5a20663960f
ESTOLEAR <stolearelena88@gmail.com>
2024-05-18 11:24:56 +0300

    init commit

---
+++ src/test/java/Tests/TestRegres.java
@@ -0,0 +1,12 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+public class TestRegres {
+    @Test
+    public void testTestNg() {
+        System.out.println("Hello World");
+
+
+    }
+}
